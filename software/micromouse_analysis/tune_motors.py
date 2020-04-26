8f80c94e170575b3126f20c54a10fa5c37a90137
Timothy Hollabaugh <timothyhollabaugh@gmail.com>
2020-04-25 22:37:23 -0400

    analysis: Plot system

--- software/micromouse_analysis/tune_motors.py
+++ software/micromouse_analysis/tune_motors.py
@@ -1,5 +1,6 @@
 from typing import *
 import control
+import matplotlib.pyplot as plt
 
 # ta: 44.8ms
 # final_v: 0.0008253731343283582 ticks per ms per output power
@@ -16,6 +17,38 @@ def calc_pid_tf(p: float, i: float, d: float):
     return p + (i / s) + (d * s)
 
 
-def tune_motors(ta: float, final_v: float) -> Tuple[float, float, float]:
+def tune_motors(ta: float, final_v: float, target: float) -> Tuple[float, float, float]:
     motor_tf = calc_motor_tf(ta, final_v)
-    pid_tf = calc_pid_tf()
+
+    print(control.zero(motor_tf))
+    print(control.pole(motor_tf))
+
+    # From manual tuning
+    #pid_tf = calc_pid_tf(7000, 0.5, 5000)
+
+    pid_tf = calc_pid_tf(10000, 0, 0)
+
+    print(pid_tf)
+
+    forward_tf = pid_tf * motor_tf
+
+    #system_tf = control.feedback(forward_tf)
+
+    system_tf = forward_tf / (1 + forward_tf)
+
+    print(system_tf)
+
+    control.pzmap(system_tf)
+    print(control.zero(system_tf))
+    print(control.pole(system_tf))
+
+    times, values = control.step_response(target * system_tf, range(0, 500))
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(times, values)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    tune_motors(1000, 0.0008253731343283582, 4)
